8b0a2c790e9aaecbb4c6849759e016a4436b6b3d
nikitawable <nikitawable312@gmail.com>
2021-03-22 17:19:49 +0530

    Substract implementation with error code

--- math.py
+++ math.py
@@ -2,7 +2,7 @@
   
  return x+y # substration implementationdef subtract(x,y):  
 
-  return x-y   # multiplication implementationdef multiply(x,y): 
+  return x-y  if y>x:		return negative_value_error    else	return -y	     # multiplication implementationdef multiply(x,y): 
   
  return x*y  # division implementationdef divide(x,y): 
 
