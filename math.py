cec9e04ec00866cd98ad688dae445f4fc475249b
nikitawable <nikitawable312@gmail.com>
2021-03-22 17:28:55 +0530

    Multiply

--- math.py
+++ math.py
@@ -3,8 +3,8 @@
  return x+y # substration implementationdef subtract(x,y):  
 
   return x-y      # multiplication implementationdef multiply(x,y): 
->>>>>>> Bug456
+
   
- return x*y  # division implementationdef divide(x,y): 
+ return (x*y)  # division implementationdef divide(x,y): 
 
    return x/y     #  Square implementation   def square(x)    return x*x
