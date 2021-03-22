ee3a125af29dd072d4c15e8acd454d84363dbe97
nikitawable <56300733+nikitawable@users.noreply.github.com>
2021-03-22 15:48:34 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,9 @@
+#implementiondef add(x,y): 
+  
+ return x+y # substration implementationdef subtract(x,y):  
+
+  return x-y   # multiplication implementationdef multiply(x,y): 
+  
+ return x*y  # division implementationdef divide(x,y): 
+
+   return x/y     #  Square implementation   def square(x)    return x*x
